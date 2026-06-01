27ee269693d92e9f5c6e627c3fc4288ebbca2125
2024125083 <choihyunjae1224@gmail.com>
2026-06-01 15:01:29 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+    int arr[20] =  {};
+    int N;
+    int *p = nullptr;
+    int *q = nullptr;
+    
+    scanf("%d",&N);
+    for(p=arr;p<arr+N;p++){
+        scanf("%d",p);
+    }
+    
+    for(q=arr;q<arr+N;q++){
+        scanf("%d",q);
+    }
+    
+    
+    for(q=arr+N-1;q>=0;q--){
+        printf("%d",*p++ + *q);
+    }
+    
+
+    return 0;
+}
